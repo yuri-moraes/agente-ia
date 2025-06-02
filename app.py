f11499211a50ef593a743c5bfcbbe3c875a48d0b
Yuri Nogueira de Moraes <yuri.nogueira.de.moraes@hotmail.com>
2025-06-02 12:58:15 -0300

    feat: new HTML and CSS files to front-end

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from pinecone.exceptions import NotFoundException
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
-from typing import List, Dict, Any, cast
+from typing import List, cast
 from pydantic import SecretStr 
 import traceback
 
