97b1513aea959ce2a9aab21fa10eacd8c603aa6b
Yuri Nogueira de Moraes <yuri.nogueira.de.moraes@hotmail.com>
2025-06-02 17:25:25 -0300

    feat: finished front-ent scripts

---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    const chatBox = document.getElementById("chat-boc");
+    const userInput = document.getElementById("user-input");
+    const sendButton = document.getElementById("send-button");
+
+    function addMessage(text, sender) {
+        const messageDiv = document.createElement("div");
+        messageDiv.classList.add("message");
+        messageDiv.classList.add(sender === "user" ? "user-message" : "system-message");
+        messageDiv.textContent = text;
+        chatBox.appendChild(messageDiv);
+        chatBox.scrollTop = chatBox.scrollHeight;
+    }
+
+    async function sendMessage() {
+        const query = userInput.value.trim();
+        if (query === "") return;
+
+        addMessage(query, "user");
+        userInput.value = "";
+
+        try {
+            const apiUrl = "http://localhost:8000/chat";
+
+            const response = await fetch(apiUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body:   JSON.stringify({query: query})
+            });
+
+            if (!response.ok) {
+                const errorData = await responde.json();
+                throw new Error(errorData.detail || "Erro ao comunicar com a API do agente.")
+            }
+
+            const data = await response.json();
+            addMessage(data.answer, "system");
+
+        } catch (error) {
+            console.error("Erro ao enviar mensagem: ", error);
+            addMessage("Desculpe, houve um erro ao processar sua solicitação. Tence novamente mais tarde.", "system");
+        }
+    }
+
+    sendButton.addEventListener("click", sendMessage);
+    userInput.addEventListener("keypress", (event)=> {
+        if (event.key === "Enter") sendMessage();
+    })
+})
